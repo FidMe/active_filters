c0bffb61b167110f3918cedb74512f517034158e
Nathan Huberty <nathan.huberty@orange.fr>
2019-02-06 10:58:13 +0100

    Gem structure

---
+++ lib/active_filters.rb
@@ -0,0 +1,6 @@
+require "active_filters/version"
+
+module ActiveFilters
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/active_filters/version.rb
@@ -0,0 +1,3 @@
+module ActiveFilters
+  VERSION = "0.1.0"
+end
